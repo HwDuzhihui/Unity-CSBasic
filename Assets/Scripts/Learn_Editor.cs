68b64475fb81f061f207ff03f9ddfafb97254072
dragon <370789792@qq.com>
2018-10-08 16:03:26 +0800

    0

---
+++ Assets/Scripts/Learn_Editor.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+//添加此脚本时增加一个BoxCollider组件
+[RequireComponent(typeof(BoxCollider))]
+
+public class Learn_Editor : MonoBehaviour {
+
+    //不显示在面板
+    [HideInInspector]
+    public int a0;
+
+    //显示在面板并序列化
+    [SerializeField]
+    private int a1;
+
+    //public默认已经[SerializeField]序列化
+    public int a2;
+
+    //右键新建一个按钮
+    [MenuItem("Assets/Button")]
+    public static void ButtonClick() { }
+    //在Create下
+    [MenuItem("Assets/Create/Button", false, 0)]
+    public static void ButtonCreateClick() { }
+    //在上方面板
+    [MenuItem("Tools/Button")]
+    public static void ButtonTopClick() { }
+}
